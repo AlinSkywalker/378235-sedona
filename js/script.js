bd6c9c6d1ee59bcd910f22e70970d455f50b6cbf
AlinSkywalker <alinskywalker@gmail.com>
2017-09-02 16:31:35 +0300

    доработки

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*var modal = document.querySelector('.modal');
+modal.classList.add('hidden');
+var button = document.querySelector('.hotel-search-button');
+button.addEventListener('click', function(e) {
+        e.preventDefault();
+        var hiddenModal=document.querySelector('.modal.hidden');
+        if(hiddenModal)
+            modal.classList.remove('hidden');
+        else
+            modal.classList.add('hidden');
+    });*/
+
+       
+
