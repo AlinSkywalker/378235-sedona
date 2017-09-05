632d6a377b4f328f315314dfcd535f18660acf33
AlinSkywalker <AlinSkywalker@gmail.com>
2017-09-05 11:13:54 +0300

    скрипты

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
-/*var modal = document.querySelector('.modal');
-modal.classList.add('hidden');
+var modal = document.querySelector('.modal');
+if(modal) modal.classList.add('hidden');
 var button = document.querySelector('.hotel-search-button');
 button.addEventListener('click', function(e) {
         e.preventDefault();
         var hiddenModal=document.querySelector('.modal.hidden');
-        if(hiddenModal)
-            modal.classList.remove('hidden');
-        else
-            modal.classList.add('hidden');
-    });*/
+        if(hiddenModal){
+          
+          modal.classList.add('modal-show');
+          modal.classList.remove('hidden');
+        }
+        else{
+          modal.classList.add('hidden');
+          modal.classList.remove('modal-show');
+        }
+    });
 
        
 
