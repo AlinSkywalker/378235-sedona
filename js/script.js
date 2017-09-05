cb5847a1543720cd128cafce0439d082215eb829
AlinSkywalker <alinskywalker@gmail.com>
2017-09-05 15:38:37 +0300

    скрипт

--- js/script.js
+++ js/script.js
@@ -1,19 +1,10 @@
+document.querySelector('body').classList.remove('no-js');
 var modal = document.querySelector('.modal');
-if(modal) modal.classList.add('hidden');
 var button = document.querySelector('.hotel-search-button');
-button.addEventListener('click', function(e) {
-        e.preventDefault();
-        var hiddenModal=document.querySelector('.modal.hidden');
-        if(hiddenModal){
-          
-          modal.classList.add('modal-show');
-          modal.classList.remove('hidden');
-        }
-        else{
-          modal.classList.add('hidden');
-          modal.classList.remove('modal-show');
-        }
-    });
-
-       
-
+button.addEventListener('click', function (e) {
+  e.preventDefault();
+  if (modal.classList.contains('modal-show'))
+    modal.classList.remove('modal-show');
+  else
+    modal.classList.add('modal-show');
+});
